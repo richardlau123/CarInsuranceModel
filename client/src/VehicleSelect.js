8d6e01882618587057c732ca2f09e7a8e6b79b5b
noahadams <adamsnoah98@gmail.com>
2019-03-26 21:17:32 -0700

    updates

--- client/src/VehicleSelect.js
+++ client/src/VehicleSelect.js
@@ -6,7 +6,7 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import VehicleView from './VehicleView'
 import Axios from 'axios';
-
+//
 class VehicleSelect extends Component{
 
     state = {
